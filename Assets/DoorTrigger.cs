e3954c0890c42a14cb4f3578cbce587cd286ed34
JamariWilliams2002 <jamwilliams02@gmail.com>
2022-05-06 17:40:24 -0400

    working door trigger

--- Assets/DoorTrigger.cs
+++ Assets/DoorTrigger.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    //grab the player 
-    GameObject player = GameManager.instance.player;
     [SerializeField]
     BoxCollider doorCollider;
-    private void FixedUpdate()
-    {
-        //if the player collides with the doortrigger
+    private int doorRoomId = 1;
+    bool doorOpen;
 
+    private void OnTriggerExit(Collider other)
+    {
+        //when the player is done touching the trigger, close the door
+        if (other.gameObject == GameManager.instance.player)
+            CloseDoor();
     }
+
+    private void CloseDoor()
+    {
+        Debug.Log("Door Will Close");
+    }
+    private void OpenDoor()
+    {
+        Debug.Log("Door will open");
+    }
+
 }
