012ce7f3e06464b8bb04604eabaecc5140ed8cc4
CameronEuston <CMEuston@student.fullsail.edu>
2022-05-06 22:50:57 -0400

    changed the red flash when hit Cameron

--- Assets/Scripts/Player/EnemyFlashRed.cs
+++ Assets/Scripts/Player/EnemyFlashRed.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class EnemyFlashRed : MonoBehaviour
 {
-    [SerializeField] GameObject enemy;
     Color collideColor = Color.red;
     Color normalColor = Color.yellow;
+
+    [SerializeField] float flashTime = .1f;
 
     private void OnCollisionEnter(Collision collider)
     {
         if(collider.gameObject.tag == "Bullet")
         {
-            StartCoroutine(FlashRed(enemy));
+            StartCoroutine(FlashRed());
         }
     }
 
-    IEnumerator FlashRed(GameObject target)
+    IEnumerator FlashRed()
     {
-        target.GetComponent<Renderer>().material.color = collideColor;
-        yield return new WaitForSeconds(0.2f);
-        target.GetComponent<Renderer>().material.color = normalColor;
+        GetComponent<Renderer>().material.color = collideColor;
+        yield return new WaitForSeconds(flashTime);
+        GetComponent<Renderer>().material.color = normalColor;
     }
 
 }
