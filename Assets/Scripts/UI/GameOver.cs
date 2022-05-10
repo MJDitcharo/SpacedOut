2207f2bbd4d3b04f0389cb76503e4fcc219d6df1
JamariWilliams2002 <jamwilliams02@gmail.com>
2022-05-10 12:04:38 -0400

    before popup menu

--- Assets/Scripts/UI/GameOver.cs
+++ Assets/Scripts/UI/GameOver.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //if the player's health is zero
+        //{
+        //
+        //}
     }
 }
