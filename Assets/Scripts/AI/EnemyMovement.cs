f861232c247ec4b3673c84e3249e1b9d15beef56
Myles Ditcharo <MJDitcharo@student.Fullsail.edu>
2022-05-24 14:45:02 -0400

    Rocket knockback / myles

--- Assets/Scripts/AI/EnemyMovement.cs
+++ Assets/Scripts/AI/EnemyMovement.cs
@@ -6,17 +6,26 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] NavMeshAgent navmeshAgent;
+    [SerializeField] float pushbackFalloffSpeed = 4;
+    public Vector3 pushback;
+
     GameObject player;
     public float movementSpeed;
 
     void Start()
     {
         player = GameManager.instance.player;
+        pushback = new Vector3(pushback.x, 0, pushback.z);
     }
-
+    private void Update()
+    {
+        pushback = Vector3.Lerp(pushback, Vector3.zero, pushbackFalloffSpeed * Time.deltaTime);
+        navmeshAgent.Move(movementSpeed * Time.deltaTime * pushback);
+    }
     public void MoveToLocation(Vector3 position)
     {
         //Debug.Log("moving to " + position);
         navmeshAgent.SetDestination(position);
     }
+    
 }
