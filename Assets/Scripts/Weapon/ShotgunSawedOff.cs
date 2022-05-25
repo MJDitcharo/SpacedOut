c358c60bb1d8109fd0449fb4692557592a615f36
Myles Ditcharo <MJDitcharo@student.Fullsail.edu>
2022-05-25 13:11:08 -0400

    WeaponUpgrades / MYles

--- Assets/Scripts/Weapon/ShotgunSawedOff.cs
+++ Assets/Scripts/Weapon/ShotgunSawedOff.cs
@@ -5,24 +5,10 @@ using UnityEngine;
 public class ShotgunSawedOff : Shotgun
 {
 
-    [SerializeField] float shotgunSpread = 45f;
-    [SerializeField] float shotgunAngleChange = 5f;
+    
 
     public override void Shoot()
     {
-        for (float angle = -shotgunSpread; angle <= shotgunSpread; angle += shotgunAngleChange)
-        {
-            GameObject bulletInstance = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); //spawn the bullet and reference the bullet to modify 
-            Rigidbody rigidbody = bulletInstance.GetComponent<Rigidbody>(); //access the rigidbody of the game object
-            bullet bulletScript = bulletInstance.GetComponent<bullet>();
-            bulletScript.damage = (int)(damage * damageMultiplier);
-            firePoint.localEulerAngles = new Vector3(0, angle, 0);
-            rigidbody.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse); //add a force in the up vector
-            GameManager.instance.bullets.Add(bulletInstance);
-        }
-
-        //deplete ammo
-        ammoCount--;
-        GameManager.instance.ammoCount.Subtract();
+        base.Shoot();
     }
 }
