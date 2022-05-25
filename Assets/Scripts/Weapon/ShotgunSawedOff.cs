804532bc6ea427a618348693b36fb03561976149
Myles Ditcharo <MJDitcharo@student.Fullsail.edu>
2022-05-25 12:17:59 -0400

    WeaponUpgrades / Myles

--- Assets/Scripts/Weapon/ShotgunSawedOff.cs
+++ Assets/Scripts/Weapon/ShotgunSawedOff.cs
@@ -2,31 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shotgun : WeaponBase
+public class ShotgunSawedOff : Shotgun
 {
-    [SerializeField] float shotgunSpread = 30f;
+
+    [SerializeField] float shotgunSpread = 45f;
     [SerializeField] float shotgunAngleChange = 5f;
-
-    private void Awake()
-    {
-        weaponID = WeaponID.Shotgun;
-    }
-
-    public override void Update()
-    {
-        if (Input.GetButtonDown("Fire1") && Time.time >= nextShotFired && ammoCount != 0 && Time.timeScale > 0) //if the first mouse button is down
-        {
-            nextShotFired = Time.time + 1f / fireRate * fireRateMultiplier; //delay for the next bullet fired
-            Shoot(); //shoot method
-        }
-    }
 
     public override void Shoot()
     {
         for (float angle = -shotgunSpread; angle <= shotgunSpread; angle += shotgunAngleChange)
         {
             GameObject bulletInstance = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); //spawn the bullet and reference the bullet to modify 
-            Rigidbody rigidbody = bulletInstance.GetComponent<Rigidbody>(); //acess the rigidbody of the game object
+            Rigidbody rigidbody = bulletInstance.GetComponent<Rigidbody>(); //access the rigidbody of the game object
             bullet bulletScript = bulletInstance.GetComponent<bullet>();
             bulletScript.damage = (int)(damage * damageMultiplier);
             firePoint.localEulerAngles = new Vector3(0, angle, 0);
