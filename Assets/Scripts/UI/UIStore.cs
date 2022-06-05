19ab08f197daf0ae55f70dd867b576d9cdc1d541
JamariWilliams2002 <94849256+JamariWilliams2002@users.noreply.github.com>
2022-06-05 11:12:43 -0400

    made uistore singleton class, moved button functions

--- Assets/Scripts/UI/UIStore.cs
+++ Assets/Scripts/UI/UIStore.cs
@@ -7,43 +7,29 @@ using UnityEngine.UI;
 public class UIStore : PopUpMenu
 {
     [SerializeField]
-    private GameObject OneShopVisual;
-    static private GameObject shopVisual;
+    private GameObject shopVisual;
     [SerializeField]
-    private GameObject purchaseMessageObj;
+    public GameObject purchaseMessageObj;
     [SerializeField]
     private GameObject firstPage;
-    static bool first = true;
-    static private GameObject statPurchaseMessage;
-    private TMPro.TextMeshProUGUI purchaseMessage;
-    private List<GameObject> pages = new();
-
+    bool first = true;
+    public TMPro.TextMeshProUGUI purchaseMessage;
+    //singleton
+    public static UIStore instance;
 
     //pickup page
-    public enum PickupCosts { Health, Ammo, Grenade, BoardWipe, Armor };
-    protected List<TMPro.TextMeshProUGUI> pickupText = new List<TMPro.TextMeshProUGUI>();
-    protected List<int> pickupCosts = new List<int>();
-
-
-    protected int boardWipeCost, healthCost, ammoCost, grenadeCost, armorCost;
-    protected TMPro.TextMeshProUGUI boardWipeCostStr, healthCostStr, ammoCostStr, grenadeCostStr, armorCoststr;
-
-    protected Dictionary<TMPro.TextMeshProUGUI, int> pickups;
-    protected enum WeaponUpgradeCosts { Pistol, Shotgun, Heavy, Rifle, Melee };
-    protected int[] defaultWeaponUpgradeCosts = { 1000, 1250, 1250, 2000, 1000 };
-    protected List<TMPro.TextMeshProUGUI> weaponText = new List<TMPro.TextMeshProUGUI>();
-    protected List<int> weaponCost = new List<int>();
-    int tempCurrentSkrap;
-
 
     private void Start()
     {
-        if (shopVisual == null)
-            shopVisual = OneShopVisual;
-        if (purchaseMessageObj == null)
-            purchaseMessageObj = statPurchaseMessage;
-        else
-            statPurchaseMessage = purchaseMessageObj;
+        if (instance == null)
+            instance = this;
+        Debug.Log(transform.name);
+        //if (shopVisual == null)
+        //    shopVisual = OneShopVisual;
+        //if (purchaseMessageObj == null)
+        //    purchaseMessageObj = statPurchaseMessage;
+        //else
+        //    statPurchaseMessage = purchaseMessageObj;
 
         //grab textmesh component for purchase message
         purchaseMessage = purchaseMessageObj.GetComponent<TMPro.TextMeshProUGUI>();
@@ -75,7 +61,6 @@ public class UIStore : PopUpMenu
         if (!GameManager.instance.pmenu.gameIsPaused)
         {
             shopVisual.SetActive(true);
-            tempCurrentSkrap = GameManager.instance.skrapCount.GetQuantity();
             GameManager.instance.menuIsActive = true;
             //GameObject.Find("Weapon Upgrades Page").gameObject.SetActive(false);
             FreezeWorld();
@@ -91,248 +76,5 @@ public class UIStore : PopUpMenu
 
     }
 
-    private void CheckPurchaseItem(ItemCount itemCount, int cost, int quantity = 1)
-    {
-        bool purchaseFailed;
-        if (GameManager.instance.skrapCount.GetQuantity() >= cost)
-        {
-            Debug.Log("quantity: " + quantity);
-
-            itemCount.Add(quantity);
-            GameManager.instance.skrapCount.Subtract(cost);
-            purchaseFailed = false;
-        }
-        else
-            purchaseFailed = true;
-        StartCoroutine(HandlePurchaseMessage(purchaseFailed));
-    }
-    private void PurchaseAmmo(string weaponName, int cost, int quantity = 0)
-    {
-        bool purchaseFailed;
-        if (GameManager.instance.skrapCount.GetQuantity() >= cost)
-        {
-            Debug.Log("You got the cash");
-            WeaponBase currentWeapon = WeaponHolder.instance.transform.Find(weaponName).GetComponent<WeaponBase>();
-            GameManager.instance.ammoCount.SetQuantity(quantity);
-            currentWeapon.AddAmmo(quantity);
-            GameManager.instance.skrapCount.Subtract(cost);
-            purchaseFailed = false;
-        }
-        else
-            purchaseFailed = true;
-        StartCoroutine(HandlePurchaseMessage(purchaseFailed));
-    }
-    private void CheckPurchaseItem(int weaponIndex, float multiplier, int cost)
-    {
-        //bool purchaseFailed;
-        ////WeaponHolder.instance.UpgradeFireRate("Pistol", 1.5f);
-        //if (GameManager.instance.skrapCount.GetQuantity() >= cost)
-        //{
-        //    WeaponHolder.instance.UpgradeDamage(weaponIndex, multiplier);
-        //    WeaponHolder.instance.UpgradeFireRate(weaponIndex, multiplier);
-        //    GameManager.instance.skrapCount.Subtract(defaultWeaponUpgradeCosts[weaponIndex]);
-        //    purchaseFailed = false;
-        //}
-        //else
-        //    purchaseFailed = true;
-        //StartCoroutine(HandlePurchaseMessage(purchaseFailed));
-    }
-
-    private void UpgradeFireRate(string weaponName, int cost, float fireRate)
-    {
-        bool purchaseFailed;
-        if (GameManager.instance.skrapCount.GetQuantity() >= cost)
-        {
-            WeaponHolder.instance.UpgradeFireRate(weaponName, fireRate);
-            GameManager.instance.skrapCount.Subtract(cost);
-            purchaseFailed = false;
-            Debug.Log("Fire rate upgraded");
-        }
-        else
-            purchaseFailed = true;
-        StartCoroutine(HandlePurchaseMessage(purchaseFailed));
-    }
-
-    private void UpgradeDamage(string weaponName, int cost, float damage)
-    {
-        bool purchaseFailed;
-        if (GameManager.instance.skrapCount.GetQuantity() >= cost)
-        {
-            WeaponHolder.instance.UpgradeDamage(weaponName, damage);
-            GameManager.instance.skrapCount.Subtract(cost);
-            Debug.Log("Damage upgraded");
-            purchaseFailed = false;
-        }
-        else
-            purchaseFailed = true;
-        StartCoroutine(HandlePurchaseMessage(purchaseFailed));
-    }
-
-    private IEnumerator HandlePurchaseMessage(bool purchaseFailed, string message = "")
-    {
-        Debug.Log(purchaseFailed);
-        if (!purchaseFailed)
-        {
-            purchaseMessage.color = Color.green;
-            message = "Transaction Success!";
-        }
-        else
-        {
-            purchaseMessage.color = Color.red;
-            message = "Transaction Failed!";
-        }
-        purchaseMessage.text = message;
-
-        statPurchaseMessage.SetActive(true);
-        yield return new WaitForSecondsRealtime(2);
-        statPurchaseMessage.SetActive(false);
-    }
-
-    #region Shop Buttons
-    public void ExitShop()
-    {
-        GameManager.instance.shopUI.Deactivate();
-    }
-
-    public void NextPage()
-    {
-
-    }
-
-    public void PreviousPage()
-    {
-
-    }
-
-    #region PickupButtons
-    // Allows you to buy anything in the shop taking away the proper amount of skrap while adding the item
-    public void BuyBoardWipe()
-    {
-        CheckPurchaseItem(GameManager.instance.boardWipeCount, GeneralPage.instance.boardWipe);
-    }
-    public void BuyGrenade()
-    {
-        CheckPurchaseItem(GameManager.instance.grenadeCount, GeneralPage.instance.grenade);
-    }
-    public void BuyArmor()
-    {
-        //armor doesn't work
-    }
-    public void BuyHealth()
-    {
-        bool purchaseFailed;
-        if (GameManager.instance.skrapCount.GetQuantity() >= GeneralPage.instance.healthCost)
-        {
-            GameManager.instance.playerHealth.AddHealth((float)(GeneralPage.instance.healthQuantity * .01f));
-            GameManager.instance.skrapCount.Subtract(GeneralPage.instance.healthCost);
-            purchaseFailed = false;
-        }
-        else
-            purchaseFailed = true;
-        StartCoroutine(HandlePurchaseMessage(purchaseFailed));
-    }
-
-    public void BuyMaxHealth()
-    {
-        bool purchaseFailed;
-        if (GameManager.instance.skrapCount.GetQuantity() >= GeneralPage.instance.maxHealth)
-        {
-            GameManager.instance.playerHealth.AddMaxHealth(10);
-            GameManager.instance.skrapCount.Subtract(GeneralPage.instance.maxHealth);
-            purchaseFailed = false;
-        }
-        else
-            purchaseFailed = true;
-        StartCoroutine(HandlePurchaseMessage(purchaseFailed));
-    }
-
-    #endregion
-
-    #region WeaponButtons
-
-    #region Pistol Page
-    public void PistolAmmo()
-    {
-        PurchaseAmmo("Pistol", PistolPage.instance.ammoCost, PistolPage.instance.ammoQuantity);
-    }
-
-    public void PistolUpgradeFireRate()
-    {
-        UpgradeFireRate("Pistol", PistolPage.instance.tier1Upgrade, PistolPage.instance.fireRateQuantity);
-    }
-
-    public void PistolUpgradeDamage()
-    {
-        UpgradeDamage("Pistol", PistolPage.instance.tier1Upgrade, PistolPage.instance.damageQuantity);
-    }
-
-    public void PistolDeagleUpgrade()
-    {
-
-    }
-
-    public void PistolDualWeildUpgrade()
-    {
-
-    }
-    public void PistolVoidUpgrade()
-    {
-
-    }
-    public void PistolPlasmaUpgrade()
-    {
-
-    }
-    #endregion
-
-    #region Shotgun Page
-    public void ShotgunBuyWeapon()
-    {
-
-    }
-
-    public void ShotgunAmmo()
-    {
-        PurchaseAmmo("Shotgun", ShotgunPage.instance.ammoCost, ShotgunPage.instance.ammoQuantity);
-    }
-
-    public void ShotgunUpgradeFireRate()
-    {
-        UpgradeFireRate("Shotgun", ShotgunPage.instance.tier1Upgrade, ShotgunPage.instance.fireRateQuantity);
-
-    }
-
-    public void ShotgunUpgradeSlug()
-    {
-
-    }
-    public void ShotgunUpgradeSawedOff()
-    {
-
-    }
-    public void ShotgunPlasmaUpgrade()
-    {
-
-    }
-
-    public void ShotgunVoidUpgrade()
-    {
-
-    }
-
-
-
-    #endregion
-    public void RifleAmmo()
-    {
-        PurchaseAmmo("Rifle", RiflePage.instance.ammoCost, RiflePage.instance.ammoQuantity);
-    }
-
-    public void HeavyAmmo()
-    {
-        PurchaseAmmo("Heavy", HeavyPage.instance.ammoCost, HeavyPage.instance.ammoQuantity);
-    }
-    #endregion
-
-    #endregion
+   
 }
