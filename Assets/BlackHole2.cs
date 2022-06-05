80b5a0461e0099631eabd649a4ca013731229e8b
JamariWilliams2002 <94849256+JamariWilliams2002@users.noreply.github.com>
2022-06-05 00:29:58 -0400

    began idea for making player a child of the gameobject

--- Assets/BlackHole2.cs
+++ Assets/BlackHole2.cs
@@ -6,10 +6,10 @@ public class BlackHole2 : MonoBehaviour
 {
 
     [SerializeField]
-    float attractForce, attractRange = 10;
+    float attractForce, attractRange, moveX, moveZ;
+    Vector3 moveDirection;
     Transform objectToMove;
     Transform player;
-
     private void Start()
     {
         player = GameManager.instance.player.transform;
@@ -18,15 +18,33 @@ public class BlackHole2 : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(Vector3.Distance(player.position, transform.position) < attractRange) //check to see if player is in range
+        if (Vector3.Distance(player.position, transform.position) < attractRange) //check to see if player is in range
         {
-            Debug.Log("sees player");
-            Debug.Log(transform.position);
-            player.Translate(((transform.position * Time.deltaTime)));
+            player.parent = transform;
+
+
+
+            Vector3 localPos = player.InverseTransformPoint(transform.position);
+            Debug.Log(localPos);
+
+            //determine which way to attract to 
+            //if (player.position.z > 0)
+            //    moveZ = attractForce;
+            //else
+            //    moveZ = -attractForce;
+
+            //if (player.position.x > 0)
+            //    moveX = attractForce;
+            //else
+            //    moveX = -attractForce;
+
+            //moveDirection = new Vector3(moveX, 0, moveZ);
+
+            //player.Translate(((moveDirection * Time.deltaTime)));
             //Vector3.MoveTowards(player.position, transform.position, attractForce * Time.deltaTime);
         }
     }
 
 
-    
+
 }
