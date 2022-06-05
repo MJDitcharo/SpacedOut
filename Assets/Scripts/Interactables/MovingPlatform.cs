be05459f13b1d948c059506c205233ba5fb3af3f
JamariWilliams2002 <jamwilliams02@gmail.com>
2022-06-04 21:29:26 -0400

    moving platforms ACTUALLY working

--- Assets/Scripts/Interactables/MovingPlatform.cs
+++ Assets/Scripts/Interactables/MovingPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaypointFollower : MonoBehaviour
+public class MovingPlatform : MonoBehaviour
 {
     [SerializeField]
     GameObject[] waypoints;
