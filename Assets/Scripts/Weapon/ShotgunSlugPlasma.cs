7649412804ec1eb529564585491b057d85cb05e6
Myles Ditcharo <MJDitcharo@student.Fullsail.edu>
2022-06-06 14:04:41 -0400

    Plasma Slug upgrade / Myles

---
+++ Assets/Scripts/Weapon/ShotgunSlugPlasma.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunSlugPlasma : Shotgun
+{
+
+    public int projectileSpeed;
+    // Start is called before the first frame update
+    public override void Shoot()
+    {
+        GameObject blast = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        blast.transform.rotation = transform.rotation;
+        Rigidbody blastRB = blast.GetComponent<Rigidbody>();
+        blastRB.velocity = transform.forward * projectileSpeed;
+    }
+}
