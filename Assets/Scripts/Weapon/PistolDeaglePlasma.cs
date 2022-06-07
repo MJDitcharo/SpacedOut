5664a3353f344554255fb1cdecc1a70a24d655ea
CameronEuston <CMEuston@student.fullsail.edu>
2022-06-07 15:14:35 -0400

    deagle plasma works
    Signed-off-by: CameronEuston <CMEuston@student.fullsail.edu>

--- Assets/Scripts/Weapon/PistolDeaglePlasma.cs
+++ Assets/Scripts/Weapon/PistolDeaglePlasma.cs
@@ -6,19 +6,24 @@ public class PistolDeaglePlasma : bullet
 {
     public int bulletAmount = 3;
     [SerializeField] GameObject splitBullet;
+    [SerializeField] float splitForce = 20;
+    public Collider ignore;
 
     public override void OnTriggerEnter(Collider other)
     {
-        //for (int i = 0; i < bulletAmount; i++)
-        //{
-            Debug.Log("here");
+        Debug.Log("here");
+        transform.Rotate(0, -30, 0);
+        for (float i = -30; i <= 30; i += 30)
+        {
             Instantiate(splitBullet, transform).GetComponent<ChildSplitBullet>().ignore = other;
             GameObject bullet = transform.GetChild(0).gameObject;
             Rigidbody rb = bullet.GetComponent<Rigidbody>();
             bullet.transform.SetParent(null);
 
-            rb.AddForce(transform.forward, ForceMode.Impulse); 
-        //}
+            rb.AddForce(transform.forward * splitForce, ForceMode.Impulse); 
+            transform.Rotate(0, 30, 0);
+        }
+        //Debug.Break();
 
         base.OnTriggerEnter(other);
     }
