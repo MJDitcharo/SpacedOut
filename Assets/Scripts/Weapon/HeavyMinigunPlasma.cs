6c99e45ae9d19920cb60f5e005678814cb749b8e
Myles Ditcharo <MJDitcharo@student.Fullsail.edu>
2022-06-07 11:44:18 -0400

    Plasma minigun / myles

--- Assets/Scripts/Weapon/HeavyMinigunPlasma.cs
+++ Assets/Scripts/Weapon/HeavyMinigunPlasma.cs
@@ -4,5 +4,40 @@ using UnityEngine;
 
 public class HeavyMinigunPlasma : bullet
 {
-    
+
+    public override void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        playerHealth playerHP = other.gameObject.GetComponent<playerHealth>();
+        if (playerHP != null && !playerHP.isDamageable)
+            return;
+
+
+        health HP = other.gameObject.GetComponent<health>();
+        if (HP != null)
+        {
+            HP.DoDamage(damage);
+        }
+
+        rb.velocity = new Vector3(0, 0, 0);
+        StartCoroutine(BulletExplosion(other));
+
+    }
+
+    IEnumerator BulletExplosion(Collider other)
+    {
+        health HP = other.gameObject.GetComponent<health>();
+        EnemyMovement MV = other.gameObject.GetComponent<EnemyMovement>();
+        yield return new WaitForSeconds(2);
+
+        if (HP != null)
+        {
+            HP.DoDamage(damage * 2);
+            MV.pushback = -pushbackMultiplier * (other.transform.position - new Vector3(Random.Range(0, 100), 0 , Random.Range(0,100))).normalized;
+        }
+
+
+        Instantiate(hitEffect, other.transform.position, Quaternion.identity); //create a bullet with no rotation at the postion 
+        Destroy(gameObject);
+    }
 }
