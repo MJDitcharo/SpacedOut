ead656a9ef2b8ecc95b4f3dc9acccb50670dff44
Myles Ditcharo <MJDitcharo@student.Fullsail.edu>
2022-06-07 15:19:37 -0400

    Void Slug finished / Myles

--- Assets/BlackHole2.cs
+++ Assets/BlackHole2.cs
@@ -6,30 +6,42 @@ public class BlackHole2 : MonoBehaviour
 {
     public float pullStrength = 0.5f;
     public float pullRange = 1f;
+    public int damage = 1;
+
+    public float tickTime = 1;
+    private float tick = 0;
 
     [SerializeField] GameObject BlackHoleCenter;
 
 
     private void Start()
     {
-        Vector3 size = new Vector3(pullRange, 0, pullRange);
+        Vector3 size = new Vector3(pullRange, 5, pullRange);
         transform.localScale = size;
+    }
+
+    private void Update()
+    {
+        tick += Time.deltaTime;
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("Contact with player");
             GameManager.instance.movement.pushback += (-pullStrength * (GameManager.instance.player.transform.position - BlackHoleCenter.transform.position).normalized);
         }
         
         if (other.tag == "Enemy")
         {
-            Debug.Log("Contact with Enemy");
 
             EnemyMovement movement = other.GetComponent<EnemyMovement>();
             movement.pushback += (-pullStrength * (other.transform.position - BlackHoleCenter.transform.position).normalized);
+            if (tick >= tickTime)
+            {
+                other.GetComponent<EnemyHealth>().DoDamage(damage);
+                tick = 0;
+            }
         }
     }
 }
