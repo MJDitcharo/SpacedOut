2cc268c77773ab2b106122bc5e4f6e86c7a7320b
CameronEuston <CMEuston@student.fullsail.edu>
2022-06-07 21:02:25 -0400

    plasma rifle working
    Signed-off-by: CameronEuston <CMEuston@student.fullsail.edu>

--- Assets/Scripts/Weapon/AssaultRiflePlasma.cs
+++ Assets/Scripts/Weapon/AssaultRiflePlasma.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssaultRiflePlasma : MonoBehaviour
+public class AssaultRiflePlasma : Rifle
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] TrailRenderer bulletTrail;
+    [SerializeField] float tracerTime = .1f;
+
+    public override void Shoot()
     {
+        RaycastHit hit;
+        if(Physics.Raycast(firePoint[0].position, firePoint[0].forward, out hit, Mathf.Infinity, layerMask))
+        {
+            StartCoroutine(SpawnTrail(bulletTrail, hit.point, tracerTime));
+            StartCoroutine(SpawnTrail(bulletTrail, hit.point, tracerTime));
+            health hp = hit.collider.gameObject.GetComponent<health>();
+            if(hp != null)
+            {
+                hp.DoDamage((int)damage);
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator SpawnTrail(TrailRenderer trail, Vector3 hitPoint, float speed)
     {
-        
+        GameObject tracer = Instantiate(bulletPrefab, firePoint[0].position, Quaternion.identity);
+
+        float time = 0;
+
+        while(time < tracerTime)
+        {
+            tracer.transform.position = Vector3.Lerp(firePoint[0].position, hitPoint, time / tracerTime);
+            time += Time.deltaTime;
+
+            yield return null;
+        }
     }
+
+    
 }
