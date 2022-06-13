e08ccad5359fe028f9347fd133e451bf06500fce
Ethan Ortega <ekicks0@gmail.com>
2022-06-13 10:38:19 -0400

    weapon scrips updated to play sound

--- Assets/Scripts/Weapon/HeavyGrenade.cs
+++ Assets/Scripts/Weapon/HeavyGrenade.cs
@@ -13,6 +13,6 @@ public class HeavyGrenade : Heavy
         //deplete ammo
         ammoCount--;
         GameManager.instance.ammoCount.Subtract();
-        AudioManager.Instance.PlaySFX("baseGun");
+        AudioManager.Instance.PlaySFX(gunshotSound);
     }
 }
